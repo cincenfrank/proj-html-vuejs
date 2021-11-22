2ef14f7d9cbb16a3d976f65298846cbb1bb8240b
cincenfrank <fpetrangeli1@gmail.com>
2021-11-22 16:16:23 +0100

    created jumbo header component

---
+++ src/components/JumboHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="jumbo container">
+    <div class="row">
+      <div class="col-5">
+        <ContextTitle :text="jumboObject.catchFrase"></ContextTitle>
+        <TitleComponent
+          :priority="1"
+          :text="jumboObject.mainTitle"
+        ></TitleComponent>
+        <div class="d-flex gap-4">
+          <ButtonComponent :text="jumboObject.button.text"></ButtonComponent>
+          <PlayButton class=""></PlayButton>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ButtonComponent from "./ButtonComponent.vue";
+import ContextTitle from "./ContextTitle.vue";
+import PlayButton from "./PlayButton.vue";
+import TitleComponent from "./TitleComponent.vue";
+export default {
+  components: { ContextTitle, TitleComponent, ButtonComponent, PlayButton },
+  name: "JumboHeader",
+  props: { jumboObject: Object },
+};
+</script>
+
+<style>
+.jumbo {
+  text-align: left;
+  padding: 200px 0;
+}
+</style>
