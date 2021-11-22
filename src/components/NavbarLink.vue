957ec7335606b88eeafdeddeab33bc82d5f9ce4f
cincenfrank <fpetrangeli1@gmail.com>
2021-11-22 16:15:49 +0100

    created navbarLink element

---
+++ src/components/NavbarLink.vue
@@ -0,0 +1,23 @@
+<template>
+  <a class="nav-link" :class="isActive ? 'active' : ''" :href="href">{{
+    text
+  }}</a>
+</template>
+
+<script>
+export default {
+  name: "NavbarLink",
+  props: {
+    isActive: Boolean,
+    // isDisabled: Boolean,
+    href: String,
+    text: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.nav-link {
+  text-transform: uppercase;
+}
+</style>
