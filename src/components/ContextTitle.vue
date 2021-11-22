4c799b5a1472762ec82076f42886298b649659af
cincenfrank <fpetrangeli1@gmail.com>
2021-11-22 16:15:15 +0100

    created context title element

---
+++ src/components/ContextTitle.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="d-flex align-items-baseline gap-2">
+    <div class="line-element"></div>
+    <h6>{{ text }}</h6>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContextTitle",
+  props: {
+    text: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.line-element {
+  border-bottom: 3px solid greenyellow;
+  width: 40px;
+}
+h6 {
+  vertical-align: bottom;
+  text-transform: uppercase;
+  color: white;
+}
+</style>
