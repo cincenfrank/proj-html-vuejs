0ae9d6a46ad9ca7c2551c8847776815fcdb475a6
cincenfrank <fpetrangeli1@gmail.com>
2021-11-22 16:17:00 +0100

    created title component

---
+++ src/components/TitleComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1 v-if="priority === 1" class="title">{{ text }}</h1>
+</template>
+
+<script>
+export default {
+  name: "TitleComponent",
+  props: {
+    text: String,
+    priority: { default: 1 },
+  },
+};
+</script>
+
+<style>
+* {
+  text-transform: uppercase;
+}
+.title {
+  font-size: 80px;
+  color: white;
+}
+</style>
