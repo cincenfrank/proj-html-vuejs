378c5ed74a903bb36f40bb36da4cddea8d462223
cincenfrank <fpetrangeli1@gmail.com>
2021-11-22 16:17:16 +0100

    created header component

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <Navbar :navbarObject="headerData.nav"></Navbar>
+    <JumboHeader :jumboObject="headerData.jumbo"></JumboHeader>
+  </header>
+</template>
+
+<script>
+import JumboHeader from "./JumboHeader.vue";
+import Navbar from "./Navbar.vue";
+export default {
+  components: { Navbar, JumboHeader },
+  name: "HeaderComponent",
+  props: { headerData: Object },
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  background-color: black;
+  background-image: url("../assets/portrait-of-young-architect-woman-on-meeting-KFZCE3A.jpg");
+  background-size: 66%;
+  background-repeat: no-repeat;
+  background-position-x: right;
+  background-position-y: top;
+  //   padding: 10px;
+}
+</style>
