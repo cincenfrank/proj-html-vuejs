45fec1e4e270884a1f898b67c987441ff6e20f6b
cincenfrank <fpetrangeli1@gmail.com>
2021-11-22 16:14:52 +0100

    created button component element

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <button class="btn btn-success">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: "ButtonComponent",
+  props: { text: String },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
