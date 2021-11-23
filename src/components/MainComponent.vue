0e91bdd3cfb4b8fa3885d43991ceead786e35f01
cincenfrank <fpetrangeli1@gmail.com>
2021-11-23 01:44:30 +0100

    added about us component to main

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,13 +1,15 @@
 <template>
   <main>
     <ServicesBlock :servicesObject="mainObject.servicesBlock"></ServicesBlock>
+    <AboutUsBlock :aboutUsObject="mainObject.aboutUsBlock"></AboutUsBlock>
   </main>
 </template>
 
 <script>
+import AboutUsBlock from "./AboutUsBlock.vue";
 import ServicesBlock from "./ServicesBlock.vue";
 export default {
-  components: { ServicesBlock },
+  components: { ServicesBlock, AboutUsBlock },
   name: "MainComponent",
   props: { mainObject: Object },
 };
