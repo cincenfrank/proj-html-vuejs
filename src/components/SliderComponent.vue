105ca0af70526620d819fc7e2039cc3fb83f37e1
cincenfrank <fpetrangeli1@gmail.com>
2021-11-24 18:09:24 +0100

    added transitions and slider logic

--- src/components/SliderComponent.vue
+++ src/components/SliderComponent.vue
@@ -12,21 +12,33 @@
       </div>
       <div class="col-1">
         <div class="d-flex gap-1 align-items-center">
-          <div class="slider-button"><i class="fas fa-arrow-left"></i></div>
-          <div class="slider-button"><i class="fas fa-arrow-right"></i></div>
+          <div class="slider-button" @click="onPreviousItem">
+            <i class="fas fa-arrow-left"></i>
+          </div>
+          <div class="slider-button" @click="onNextItem">
+            <i class="fas fa-arrow-right"></i>
+          </div>
         </div>
       </div>
     </div>
     <div class="row justify-content-between slider-content align-items-center">
       <div class="col-12">
         <div class="d-flex overflow-hidden gap-4">
-          <SliderCard
+          <!-- <SliderCard
             v-for="(item, i) in sliderObject.itemList"
             :key="i + '-sliderItem'"
             :category="item.category"
             :imageSrc="item.image"
             :title="item.title"
             :imageAlt="item.alt"
+          ></SliderCard> -->
+          <SliderCard
+            v-for="(index, i) in itemIndexList"
+            :key="i + '-sliderItem'"
+            :category="sliderObject.itemList[index].category"
+            :imageSrc="sliderObject.itemList[index].image"
+            :title="sliderObject.itemList[index].title"
+            :imageAlt="sliderObject.itemList[index].alt"
           ></SliderCard>
         </div>
       </div>
@@ -44,7 +56,34 @@ export default {
     sliderObject: Object,
   },
   data() {
-    return {};
+    return {
+      itemToShow: 3,
+      currentMaxItemIndex: 2,
+    };
+  },
+  computed: {
+    startingMaxItemIndex() {
+      return this.itemToShow - 1;
+    },
+    itemIndexList() {
+      const toReturn = [];
+      for (let index = 1; index <= this.itemToShow; index++) {
+        toReturn.push(this.currentMaxItemIndex - this.itemToShow + index);
+      }
+      return toReturn;
+    },
+  },
+  methods: {
+    onNextItem() {
+      if (this.currentMaxItemIndex < this.sliderObject.itemList.length - 1) {
+        this.currentMaxItemIndex++;
+      }
+    },
+    onPreviousItem() {
+      if (this.currentMaxItemIndex > this.startingMaxItemIndex) {
+        this.currentMaxItemIndex--;
+      }
+    },
   },
 };
 </script>
@@ -62,6 +101,7 @@ export default {
     background-color: $bg-grey;
     padding: 10px;
     color: white;
+    cursor: pointer;
     // display: inline-block;
   }
 }
